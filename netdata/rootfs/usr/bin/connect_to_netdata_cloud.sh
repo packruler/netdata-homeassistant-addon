bd17acf19ab6b2a8f438e5c9c117df3b4f54e4a5
Ethan Leisinger <packruler@gmail.com>
2023-01-31 17:31:52 -0700

    Update command to ignore empty params

--- netdata/rootfs/usr/bin/connect_to_netdata_cloud.sh
+++ netdata/rootfs/usr/bin/connect_to_netdata_cloud.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bashio
 
+bashio::log.info "Waiting 10 seconds for Netdata to start..."
 sleep 10 # Wait for 10 second for Netdata to start
 
 NETDATA_HOSTNAME=$(bashio::config 'hostname')
@@ -7,4 +8,24 @@ NETDATA_CLOUD_URL=$(bashio::config 'claim_url')
 NETDATA_CLOUD_TOKEN=$(bashio::config 'claim_token')
 NETDATA_CLOUD_ROOMS=$(bashio::config 'claim_rooms')
 
-sh /tmp/netdata-kickstart.sh --claim-hostname ${NETDATA_HOSTNAME} --claim-token ${NETDATA_CLOUD_TOKEN} --claim-rooms "${NETDATA_CLOUD_ROOMS}" --claim-url ${NETDATA_CLOUD_URL}
+COMMAND = ("sh /tmp/netdata-kickstart.sh")
+
+if [ -n ${NETDATA_HOSTNAME} ]; then
+  COMMAND+="--claim-hostname ${NETDATA_HOSTNAME}"
+fi
+
+if [ -n ${NETDATA_CLOUD_TOKEN} ]; then
+  COMMAND+="--claim-token ${NETDATA_CLOUD_TOKEN}"
+fi
+
+if [ -n ${NETDATA_CLOUD_ROOMS} ]; then
+  COMMAND+="--claim-rooms "${NETDATA_CLOUD_ROOMS}""
+fi
+
+if [ -n ${NETDATA_CLOUD_URL} ]; then
+  COMMAND+="--claim-url "${NETDATA_CLOUD_URL}""
+fi
+
+bashio::log.info "Executing update command: ${COMMAND[@]}"
+
+"${COMMAND[@]}"
